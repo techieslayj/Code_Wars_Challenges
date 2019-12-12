93dbdbc9f5fab87809c457ef5d83060b77394904
AJ Iglesias <ajigle1995@yahoo.com>
2019-12-12 10:56:24 -0500

    Code File

---
+++ Python/Categorize_New_Member.py
@@ -0,0 +1,18 @@
+""" 
+The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your help with an application form that will tell prospective members which category they will be placed.
+
+To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
+
+Input:
+
+Input will consist of a list of lists containing two items each. Each list contains information for a single potential member. Information consists of an integer for the person's age and an integer for the person's handicap.
+
+Note for F#: The input will be of (int list list) which is a List<List>
+"""
+
+def openOrSenior(data):
+
+	#List Comprehension Solution
+
+	return ['Senior' if person[0] >= 55 and person[1] > 7 else 'Open' for person in data]
+
